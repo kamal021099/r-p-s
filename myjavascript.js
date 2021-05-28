7cb4bdd1363ab2c310abe4cc2ac295c097d96e4a
Kamal <kamal021099@gmail.com>
2021-05-29 02:28:57 +0530

    added score and alert on winning, yet to reset the score on end

--- myjavascript.js
+++ myjavascript.js
@@ -23,15 +23,9 @@ function computerPlay()
 let computerSelection;
 let playerSelection;
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        playerSelection = (button.getAttribute("id"));
-        computerSelection = computerPlay().toUpperCase();
-        console.log(playRound(playerSelection, computerSelection));
-    });
-});
-
+//to keep count of wins;
+let compwin=0;
+let playerwin=0;
 
 function playRound(playerSelection, computerSelection)
 {
@@ -48,12 +42,53 @@ function playRound(playerSelection, computerSelection)
     }
     else if((x=="ROCK" && y=="SCISSORS") || (x=="PAPER" && y=="ROCK") || (x=="SCISSORS" && y=="PAPER"))
     {
+        compwin += 1;
         return "you lose";
     }
     else 
     {
+        playerwin += 1;
         return ("you win");
     }
-
 }
+
+//to check if the person has won or the computer in a game of 5 rounds.
+function winCheck(compwin, playerwin)
+{
+    if(compwin==5)
+    {
+        alert("you lost the game");   
+    }
+    else if(playerwin==5)
+    {
+        alert("you won the game");
+    }
+}
+
+//dynamically adding a div to show score
+const body = document.querySelector("body")
+const div1 = document.createElement('div');
+const div2 = document.createElement('div');
+div1.textContent = (compwin);
+div2.textContent = (playerwin);
+
+body.appendChild(div1);
+body.appendChild(div2);
+
+
+//on click parameters are assigned and one round is played.
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = (button.getAttribute("id"));
+        computerSelection = computerPlay().toUpperCase();
+        console.log(playRound(playerSelection, computerSelection));
+        winCheck(compwin,playerwin);
+        div1.textContent = (compwin);
+        div2.textContent = (playerwin);
+    });
+});
+
+
+
 
